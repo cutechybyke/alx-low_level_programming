b696e24079c9bfbe98f47c8de9fff27efd9d9eff
Obinna Nwaneri <chibuikeobinna3@gmail.com>
2022-12-09 15:40:46 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,15 @@ int main(void)
 	int ones = '0';
 	int tens = '0';
 
-	for (tens = '0'; tens <= '0'; tens++)
+	for (tens = '0'; tens <= '9'; tens++)
 	{
-		for (ones = '0'; ones <= '0'; ones++)
+		for (ones = '0'; ones <= '9'; ones++)
 		{
 			if (!((ones == tens) || (tens > ones)))
 			{
 				putchar(tens);
 				putchar(ones);
-				if (!(ones == '0' && tens == '0'))
+				if (!(ones == '9' && tens == '8'))
 				{
 					putchar(',');
 					putchar(' ');
