d6adacc0b3f6b9190c94d1e96ff9891f1ac7f548
Obinna Nwaneri <chibuikeobinna3@gmail.com>
2022-12-13 15:25:01 -0800

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _lower - checks if a character is lower case
+ * @c: the chracter
+ * Return: 1 if letter is lowercase, 0 if not lowercase
+ */
+int _lower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
