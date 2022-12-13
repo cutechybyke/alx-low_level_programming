2be0e573ac06d24942cfee30e4a1835c2c64081d
Obinna Nwaneri <chibuikeobinna3@gmail.com>
2022-12-13 14:29:46 -0800

    nests

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
-#include <stdio.h>
-#include <main.h>
+#include "main.h"
 
 /**
- * main -  prints putchar
+ * main - prints _putchar
  * Return: Always 0
  */
-int main();
+int main(void)
 {
-	int i;
-
-	int main()
-	{
-		putcharf("_putchar" "%d\n");
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
