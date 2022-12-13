54a595e4bc09f2d1b2e7e9af878849847117e06d
Obinna Nwaneri <chibuikeobinna3@gmail.com>
2022-12-13 14:59:23 -0800

    void

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *  print me alphabets - prints the aplabets in lowercase
+ */
+void print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n');
+}
