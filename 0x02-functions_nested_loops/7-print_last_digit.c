fe23f07aea83619118f960a197abcc9db07286bc
Obinna Nwaneri <chibuikeobinna3@gmail.com>
2022-12-13 16:39:42 -0800

    last

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digits - prints last no
+ * @n: the no
+ * Return: value of last digit
+ */
+int print_last_digit(int n)
+{
+	int ld = n % 10;
+
+	if (ld < 10)
+		ld *= -1;
+
+	_putchar(ld + '0');
+
+	return (0);
+}
