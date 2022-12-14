266ce81a661817fe7d0da6432de1b1132556d66c
Obinna Nwaneri <chibuikeobinna3@gmail.com>
2022-12-13 22:06:34 -0800

    asd

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,10 +9,10 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void print_times_table(void);
+void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 int _putchar(char c);
-
+void print_times_table(int n);
 
 #endif
