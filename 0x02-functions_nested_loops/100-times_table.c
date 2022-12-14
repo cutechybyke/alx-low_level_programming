f917b03e34408052f8a36b91c8c9ecaf366b1f82
Obinna Nwaneri <chibuikeobinna3@gmail.com>
2022-12-13 20:29:12 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,10 +4,9 @@
  * print_times_table - prints the n times
  * @n: number of the times table
  */
-void print_times_table(int n)
+void print_times_table(int n);
 {
 	int i, j, k;
-
 
 	if (n >= 0 && n <= 15)
 	{
