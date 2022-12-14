addb907c07e49dbd24f15ec41b3eac6d80985f40
Obinna Nwaneri <chibuikeobinna3@gmail.com>
2022-12-13 16:23:39 -0800

    isme

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: the number of thesig to be printed
+ * Return: 1 if no is greater than zero, print 0, otherwise -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar ('0');
+		return (0);
+	}
+	else
+	{
+		_putchar ('-');
+		return (-1);
+	}
+}
