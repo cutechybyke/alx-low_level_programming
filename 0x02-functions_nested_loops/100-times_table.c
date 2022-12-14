2dbc756fcbfb2f4e8c07ffc9f5fcc4184018df41
Obinna Nwaneri <chibuikeobinna3@gmail.com>
2022-12-13 21:52:18 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,7 +30,6 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
 				} else if (k >= 100)
