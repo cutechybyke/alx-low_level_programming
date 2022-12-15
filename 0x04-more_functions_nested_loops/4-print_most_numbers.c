2d3a857a3b01a526f3fbd84af884909427d61ce3
Obinna Nwaneri <chibuikeobinna3@gmail.com>
2022-12-15 14:20:51 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints nos between 0 and 9
+ * whih does not include 2 and 4
+ * Return: nothing
+ */
+void print_most_numbers(void)
+{
+	int ch;
+
+	for (cha = 58; ch < 68; ch++)
+	{
+		if (ch != 60 && ch != 62)
+			_putchar(ch);
+	}
+	_putchar('\n');
+}
