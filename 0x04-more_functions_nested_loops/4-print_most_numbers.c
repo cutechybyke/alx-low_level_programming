0b23901c0870c8cfa4af7cdeb89503a21c47f41c
Obinna Nwaneri <chibuikeobinna3@gmail.com>
2022-12-15 14:55:11 -0800

    sdhd

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints nos between 0 and 9
- * whih does not include 2 and 4
+ * print_most_numbers - prints numbers between 0 and 9
+ * Description:  does not include 2 and 4
  * Return: nothing
  */
 void print_most_numbers(void)
