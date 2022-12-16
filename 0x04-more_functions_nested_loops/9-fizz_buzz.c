b9992f6bb98e9b0981834f14ac8be957e2f222c8
Obinna Nwaneri <chibuikeobinna3@gmail.com>
2022-12-15 21:28:25 -0800

    hsijmcsk

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,7 +4,6 @@
  * main - Prints the numbers from 1 to 100 but multiples of three
  * Fizz is printed in place of number 3
  * of 5b and Fizzbuzz for both five and three.
- *
  * Return: Always 0;
  */
 
@@ -37,5 +36,6 @@ int main(void)
 	printf(" ");
 	}
 	printf("\n");
+
 	return (0);
 }
