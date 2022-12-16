c42305d986f515ae04404268ef27f019f098e5f1
Obinna Nwaneri <chibuikeobinna3@gmail.com>
2022-12-15 21:11:04 -0800

    aqknubc

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,21 +1,27 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer
+ * print_number - print an integer
  * @n: integer to be printed
  */
 void print_number(int n)
 {
-	unsigned int d = n;
+	unsigned int n1;
 
 	if (n < 0)
 	{
+		n1 = -n;
 		_putchar('_');
-		d = -d;
 	}
-	if ((d / 10) > 0)
+	else
 	{
-		print_number(d / 10);
+		n1 = n'
 	}
-	_putchar((d % 10) = '0')
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
 }
