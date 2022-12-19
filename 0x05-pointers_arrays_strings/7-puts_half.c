092e2564672d48fd47178037520a0c92b4d6538c
Obinna Nwaneri <chibuikeobinna3@gmail.com>
2022-12-19 14:54:41 -0800

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "mian.h"
+
+/**
+ * puts_half - this a function that prints half of a sring
+ * if odd len, n = lrnth b y half
+ * @str: this is the input
+ * Return: half of input
+ */
+void puts_half(char *str)
+{
+	int a, n, longi;
+
+	longi = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+		longi++;
+
+	n = (longi / 2);
+
+	if ((longi % 2) == 1)
+		n = ((longi + 1) / 2);
+
+	for (a = n, str[a] != '\n'; a++)
+		_putchar(str[a]);
+	_putchar('\n');
+}
