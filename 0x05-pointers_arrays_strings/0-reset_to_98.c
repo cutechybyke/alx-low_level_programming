604b34eec7b3df30761a4c8b399be568a2196d6c
Obinna Nwaneri <chibuikeobinna3@gmail.com>
2022-12-19 13:30:28 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that   takes a pointe to an int as a parameter
+ * and updtaes the values it points to, to 98
+ * @n: input
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+read me for thei sproject directory
