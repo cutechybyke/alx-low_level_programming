7c2ab971128a4b44417a8884360a9e646284cabc
Obinna Nwaneri <chibuikeobinna3@gmail.com>
2022-12-19 15:00:50 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -20,7 +20,7 @@ void puts_half(char *str)
 	if ((longi % 2) == 1)
 		n = ((longi + 1) / 2);
 
-	for (a = n; str[a] != '\n'; a++)
+	for (a = n; str[a] != '\0'; a++)
 		_putchar(str[a]);
 	_putchar('\n');
 }
