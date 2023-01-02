9c0608917b22d11ecd8c791e712c7310b91fe056
Obinna Nwaneri <chibuikeobinna3@gmail.com>
2023-01-02 14:59:54 -0800

    sjnxisihiif

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -18,7 +18,7 @@ void print_diagsums(int *a, int size)
 	{
 		sum1 = sum1 + a[y * size + y];
 	}
-	for (y = size - 1; y > 0; y--)
+	for (y = size - 1; y >= 0; y--)
 	{
 		sum2 += a[y * size + (size - y - 1)];
 	}
