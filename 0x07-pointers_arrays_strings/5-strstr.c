9ed5f336444593786061022823efc736eff14d66
Obinna Nwaneri <chibuikeobinna3@gmail.com>
2023-01-02 14:21:16 -0800

    ducnuhcs

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *I = haystack;
+		char *p = needle;
+
+		while (*I == *p && *p != '\0')
+		{
+			I++;
+			p++;
+		}
+		if (*p == '\0')
+			return (haystack);
+	}
+	return (0);
+}
