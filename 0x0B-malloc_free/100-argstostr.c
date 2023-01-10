8b22a9ee150d0108a6ef620abf21a11e4f0e739f
Obinna Nwaneri <chibuikeobinna3@gmail.com>
2023-01-10 13:30:27 -0800

    ssksss

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -14,17 +14,20 @@ char *argstostr(int ac, char **av)
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
+
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 			len++;
 		len++;
 	}
+
 	str = malloc(sizeof(char) * (len + 1));
 
 	if (str == NULL)
 		return (NULL);
 	k = 0;
+
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
