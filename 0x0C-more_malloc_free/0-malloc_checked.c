6b75689c9928b17c5b24b974bfd363f8279b9e0c
Obinna Nwaneri <chibuikeobinna3@gmail.com>
2023-01-12 12:18:19 -0800

    ssdd

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
-
 /**
- * malloc_checked - Allocates memory using malloc.
- * @b: The number of bytes to be allocated.
- *
- * Return: A pointer to the allocated memory.
+ * malloc_checked - array for prints a string
+ * @b: number of memory
+ * Return: void
  */
 void *malloc_checked(unsigned int b)
 {
-	void *mem = malloc(b);
+	void *p;
 
-	if (mem == NULL)
+	p = malloc(b);
+
+	if (p == NULL)
+	{
 		exit(98);
-
-	return (mem);
+	}
+	return (p);
 }
