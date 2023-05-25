02063ea98f55f007dc6e5e9ecc41edcbffd64dac
Obinna Nwaneri <chibuikeobinna3@gmail.com>
2023-05-24 17:50:31 -0700

    makefiles

---
+++ 0x1C-makefiles/README.md
@@ -0,0 +1,2 @@
+This is for this project and all that it encompasses.
+I love this type of projects
---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,13 @@
+#include "m.h"
+
+/**
+ * Main - Entry point
+ *
+ * Return: Always 
+ */
+int main(void)
+{
+	print_school();
+	return (EXIT_SUCCESS);
+}
+/* school */
